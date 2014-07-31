3b14b3bc7eb3d31272b702013a4d27f8eb555b24
Pieter Vantorre <pietervantorre@gmail.com>
2014-07-31 22:31:22 +0200

    Added basic gui for rendering the grid.

--- implementation/window/window.go
+++ implementation/window/window.go
@@ -2,12 +2,13 @@ package window
 
 import (
 	"fmt"
-	/*"github.com/mattn/go-gtk/glib"
-	"github.com/mattn/go-gtk/gtk"*/
+	"github.com/mattn/go-gtk/gdk"
+	"github.com/mattn/go-gtk/glib"
+	"github.com/mattn/go-gtk/gtk"
 )
 
 func Init(title string) {
-	/*gtk.Init(nil)
+	gtk.Init(nil)
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
 	window.SetPosition(gtk.WIN_POS_CENTER)
 	window.SetTitle(title)
@@ -15,6 +16,62 @@ func Init(title string) {
 	window.Connect("destroy", func(ctx *glib.CallbackContext) {
 		fmt.Println("got destroy!", ctx.Data().(string))
 		gtk.MainQuit()
-	}, "foo")*/
-	fmt.Println(title)
+	}, "foo")
+
+	//--------------------------------------------------------
+	// GtkVBox
+	//--------------------------------------------------------
+	vbox := gtk.NewVBox(false, 1)
+
+	//--------------------------------------------------------
+	// GtkMenuBar
+	//--------------------------------------------------------
+	menubar := gtk.NewMenuBar()
+	vbox.PackStart(menubar, false, false, 0)
+
+	//--------------------------------------------------------
+	// GtkDrawable
+	//--------------------------------------------------------
+
+	drawingarea := gtk.NewDrawingArea()
+	//var gdkwin *gdk.Window
+	var pixmap *gdk.Pixmap
+	var gc *gdk.GC
+	drawingarea.Connect("configure-event", func() {
+		println("Configuring drawingArea!")
+		if pixmap != nil {
+			pixmap.Unref()
+		}
+		allocation := drawingarea.GetAllocation()
+		pixmap = gdk.NewPixmap(drawingarea.GetWindow().GetDrawable(), allocation.Width, allocation.Height, 24)
+		gc = gdk.NewGC(pixmap.GetDrawable())
+		gc.SetRgbFgColor(gdk.NewColor("white"))
+		pixmap.GetDrawable().DrawRectangle(gc, true, 0, 0, -1, -1)
+		gc.SetRgbFgColor(gdk.NewColor("black"))
+		gc.SetRgbBgColor(gdk.NewColor("white"))
+		pixmap.GetDrawable().DrawRectangle(gc, false, 0, 0, 10, 10)
+	})
+
+	drawingarea.Connect("expose-event", func() {
+		println("Exposing DrawingArea!")
+		if pixmap != nil {
+			drawingarea.GetWindow().GetDrawable().DrawDrawable(gc, pixmap.GetDrawable(), 0, 0, 0, 0, -1, -1)
+		}
+	})
+
+	vbox.Add(drawingarea)
+
+	//--------------------------------------------------------
+	// GtkScale
+	//--------------------------------------------------------
+	scale := gtk.NewHScaleWithRange(0, 100, 1)
+	scale.Connect("value-changed", func() {
+		//fmt.Println("scale:", int(scale.GetValue()))
+	})
+	vbox.Add(scale)
+
+	window.Add(vbox)
+	window.SetSizeRequest(600, 600)
+	window.ShowAll()
+	gtk.Main()
 }
--- publicInterface.go
+++ publicInterface.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	window.Init("Test")
+	window.Init("TestWindow")
 }
